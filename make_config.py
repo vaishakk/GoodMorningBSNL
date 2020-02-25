5d45153d90e80d8d1713dd4eb94c8ac902931acf
Vaishak K <34245366+vaishakk@users.noreply.github.com>
2020-02-25 08:51:42 +0530

    Update make_config.py

--- make_config.py
+++ make_config.py
@@ -18,11 +18,11 @@ def make_config(task, *argv):
              Usage: make_config('set_oper', <Circle code>, <SSA name>, <List of exchanges>)
              Ex: make_config('set_oper', 'KL', 'THIRUVANANTHAPURAM', ['TVMKVT', 'TVMTPK'])
           3. edit_creds - Change ITPC username and password.
-             Usage: make_config('set_creds', <username>, <password>)
-             Ex: make_config('set_creds', 'B20xxxxxxx', 'B20xxxxxxx')
+             Usage: make_config('edit_creds', <username>, <password>)
+             Ex: make_config('edit_creds', 'B20xxxxxxx', 'B20xxxxxxx')
           4. edit_oper - Change operational parameters like Circle, SSA and Exchanges.
-             Usage: make_config('set_oper', <Circle code>, <SSA name>, <List of exchanges>)
-             Ex: make_config('set_oper', 'KL', 'THIRUVANANTHAPURAM', ['TVMKVT', 'TVMTPK'])
+             Usage: make_config('edit_oper', <Circle code>, <SSA name>, <List of exchanges>)
+             Ex: make_config('edit_oper', 'KL', 'THIRUVANANTHAPURAM', ['TVMKVT', 'TVMTPK'])
     """
     if task == 'set_creds':
         config.add_section('CREDENTIALS')
