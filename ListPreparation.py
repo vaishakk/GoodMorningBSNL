f39fc651cc6a2a2673735c92a3ba36c6374fec44
Vaishak <vaishakk@gmail.com>
2020-02-27 17:46:18 +0530

    Change output filename based on current time.

--- ListPreparation.py
+++ ListPreparation.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+from datetime import datetime
 
 
 def prepare_list(exgs=[], cols=[], filename=''):
@@ -19,7 +20,8 @@ def write_list(exgs=[], cols=[]):
     ll_file = os.path.join(download_dir+'\Pending faults Details.csv')
     ftth_list = prepare_list(exgs=exgs, cols=ftth_cols, filename=ftth_file)
     ll_list = prepare_list(exgs=exgs, cols=ll_cols, filename=ll_file)
-    dl_path = os.path.join(os.environ['userprofile'], 'Desktop\\GM.xlsx')
+    current_time = datetime.now().strftime("%d_%m_%Y_%H%M%S")
+    dl_path = os.path.join(os.environ['userprofile'], 'Desktop\\Faults_{}.xlsx'.format(current_time))
     with pd.ExcelWriter(dl_path) as writer:
         ftth_list.to_excel(writer, 'FTTH')
         ll_list.to_excel(writer, 'LL+BB')
