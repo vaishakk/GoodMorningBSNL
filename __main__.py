2513c1ae81e018e496033872d6a7f581f970b133
Vaishak <vaishakk@gmail.com>
2020-02-27 17:28:46 +0530

    Ask user for config info on first run.

--- __main__.py
+++ __main__.py
@@ -15,17 +15,35 @@ def main():
         ssa = config['OPERATIONAL PARAMETERS']['ssa']
         exgs = config['OPERATIONAL PARAMETERS']['Exchanges'].split(',')
         print(exgs)
-
         browser = ITPCBrowser(username, password)
         browser.set_params(circle=circle, ssa=ssa, exgs=exgs)
         browser.pipeline()
         ListPreparation.write_list(exgs=exgs)
     else:
-        print("Username and password not set. Run python make_config.py <username> <password>")
+        print("ITPC username and password and exchange codes need to set before starting."
+              " PRESS ANY KEY to continue...")
+        # _ = input()
+        username = input("Enter ITPC Username: ")
+        password = input("Enter ITPC password: ")
+        make_config.make_config('set_creds', username, password)
+        print("Enter circle code, ssa name and exchange codes one-by-one as given in ITPC (Ex: For "
+              "Kariyavattom exchange Circle code is KL, SSA name is THIRUVANANTHAPURAM and exchange code is TVMKVT)")
+        circle = input('Enter Circle code: ')
+        ssa = input('Enter SSA name: ')
+        exgs = ''
+        exg = ' '
+        k = 0
+        while exg != '':
+            exg = input('Enter exchange code {}: '.format(k+1))
+            k += 1
+            exgs += exg + ','
+        exgs = exgs[:-2] # Remove the blank entry
+        make_config.make_config('set_oper', circle, ssa, exgs)
+        main()
         return
 
 
 if __name__ == "__main__":
-    #main()
-    make_config.make_config('edit_creds', 'B201001415', 'B201001415')
-    #make_config.make_config('edit_oper', 'KL', 'THIRUVANANTHAPURAM', ['TVMMNK', 'TVMTPK'])
+    main()
+    # make_config.make_config('edit_creds', 'B201001415', 'B201001415')
+    # make_config.make_config('edit_oper', 'KL', 'THIRUVANANTHAPURAM', ['TVMMNK', 'TVMTPK'])
